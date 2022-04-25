98456cdf1c83b3917d7a484fe7c6cd0327f6d439
Rui Costa <ram666.rc@gmail.com>
2022-04-25 15:48:22 +0100

    Formatting values

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -35,9 +35,16 @@ export function TransactionsTable() {
 					{transactions.map(transaction => (
 						<tr key={transaction.id}>
 							<td>{transaction.title}</td>
-							<td className={transaction.type}>{transaction.amount}</td>
+							<td className={transaction.type}>
+								{new Intl.NumberFormat('pt-PT', {
+									style: 'currency',
+									currency: 'EUR',
+								}).format(transaction.amount)}
+							</td>
 							<td>{transaction.category}</td>
-							<td>{transaction.createdAt}</td>
+							<td>
+								{new Intl.DateTimeFormat('pt-PT', {}).format(new Date(transaction.createdAt))}
+							</td>
 						</tr>
 					))}
 				</tbody>

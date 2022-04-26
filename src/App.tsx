ffa438afd9b57c7cb5b90b1552be755d6d946c3a
Rui Costa <ram666.rc@gmail.com>
2022-04-26 11:18:12 +0100

    Add react context

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { GlobalStyle } from './styles/global';
 
@@ -21,7 +22,7 @@ export function App() {
 	}
 
 	return (
-		<>
+		<TransactionsProvider>
 			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 			<Dashboard />
 			<NewTransactionModal
@@ -30,6 +31,6 @@ export function App() {
 			/>
 
 			<GlobalStyle />
-		</>
+		</TransactionsProvider>
 	);
 }
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
+
+import { TransactionsContext } from '../../TransactionsContext';
 
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { api } from '../../services/api';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
@@ -14,22 +15,28 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+	const { createTransaction } = useContext(TransactionsContext);
+
 	const [title, setTitle] = useState('');
-	const [value, setValue] = useState(0);
+	const [amount, setAmount] = useState(0);
 	const [category, setCategory] = useState('');
 	const [type, setType] = useState('deposit');
 
-	function handleCreateNewTransaction(e: FormEvent) {
+	async function handleCreateNewTransaction(e: FormEvent) {
 		e.preventDefault();
 
-		const data = {
+		await createTransaction({
 			title,
-			value,
+			amount,
 			category,
 			type,
-		};
+		});
 
-		api.post('/transactions', data);
+		setTitle('');
+		setAmount(0);
+		setCategory('');
+		setType('deposit');
+		onRequestClose();
 	}
 
 	return (
@@ -56,8 +63,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 				<input
 					type='number'
 					placeholder='Price'
-					value={value}
-					onChange={e => setValue(Number(e.target.value))}
+					value={amount}
+					onChange={e => setAmount(Number(e.target.value))}
 				/>
 
 				<TransactionTypeContainer>
